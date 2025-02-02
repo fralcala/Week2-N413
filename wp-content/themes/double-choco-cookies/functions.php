dbee08694bd9c0339d98385c342906ff9cf69850
fralcala <fralcala@iu.edu>
2025-02-01 20:47:17 -0800

    added double choco cookie child theme to exports folder

---
+++ wp-content/themes/double-choco-cookies/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function my_child_theme_assets()
+{
+    wp_enqueue_style(
+        "parent-style",
+        get_template_directory_uri() . "/style.css"
+    );
+
+    wp_enqueue_style("child-style", get_stylesheet_uri());
+}
+
+add_action(
+    "wp_enqueue_scripts",
+    "my_child_theme_assets"
+);
